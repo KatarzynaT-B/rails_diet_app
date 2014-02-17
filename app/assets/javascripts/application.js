5189814d18f80fd01b8840cd22c9e62f944e2daa
Katarzyna Turbiasz <kturbiasz@gmail.com>
2014-02-17 12:06:20 +0100

    dynamic ingredient forms - blocking last ingredient removal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,25 +16,44 @@
 //= require_tree .
 //
 $(document).ready(function() {
+    if ($('.ingredient_fields').length == 1) {
+        $('.remove_ingredient_fields').hide();
+    }
+
     $('form').on('click', '.add_ingredient_fields', function(event) {
-        var ingredientFields = $('.ingredient_fields');
-        var currentDishIngredients = ingredientFields.last();
-        var ingredientTemplate = '<div class="ingredient_fields">'+ currentDishIngredients.html() + '</div>';
-        var ingredientNumber = ingredientFields.length;
-        ingredientTemplate = ingredientTemplate.replace(/_\d+_/g, '_' + ingredientNumber + '_').replace(/\[\d+\]/g, '[' + ingredientNumber + ']');
+        var ingredientFields = $('.ingredient_fields'),
+            currentDishIngredients = ingredientFields.last(),
+            ingredientTemplate = '<div class="ingredient_fields">'+ currentDishIngredients.html() + '</div>',
+            ingredientNumber = ingredientFields.length;
+        ingredientTemplate = ingredientTemplate
+            .replace(/_\d+_/g, '_' + ingredientNumber + '_')
+            .replace(/\[\d+\]/g, '[' + ingredientNumber + ']');
         $('.ingredient_form_fields').append(ingredientTemplate);
         if ($('form').hasClass('edit_dish')) {
             var uniqueId = new Date().getTime();
             var hiddenField = "<input id=\"dish_ingredients_attributes_" + ingredientNumber + "_id\" type=\"hidden\" value=\"" + uniqueId + "\" name=\"dish[ingredients_attributes][" + ingredientNumber + "][id]\">";
             $('.ingredient_form_fields').append(hiddenField);
         }
+        var linkToRemove = $('.remove_ingredient_fields')
+        if (linkToRemove.is(":hidden") == true) {
+            linkToRemove.show();
+        }
         event.preventDefault();
     });
 
-    $('.ingredient_fields').on('click', '.remove_ingredient_fields', function(event) {
-        var currentIngredientFields = $(this).closest('.ingredient_fields');
-        currentIngredientFields.find('input[type=number]').val(0);
-        currentIngredientFields.hide();
+    $('.ingredient_form_fields').on('click', '.remove_ingredient_fields', function(event) {
+        var currentIngredientFields = $(this).parent('.ingredient_fields');
+        currentIngredientFields
+            .find('input[type=number]')
+                .val(0)
+                .end()
+            .next('input[type=hidden]')
+                .remove()
+                .end()
+            .remove();
+        if ($('.ingredient_fields').length == 1) {
+            $('.remove_ingredient_fields').hide();
+        }
         event.preventDefault();
     });
 });
